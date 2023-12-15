a14b897d1f9ad0449ab7ce25d69b875c1e97643e
John Stoveld <js@USSRSMMPRI5C9B2.localdomain>
2023-12-15 16:37:49 -0500

    Getting access to our calendar.

--- main.py
+++ main.py
@@ -1,24 +1,51 @@
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 from datetime import datetime, timedelta
-import dateutil.parser
+import pytz,dateutil.parser, os
+import pickle
+from dotenv import load_dotenv
 
-# Load credentials and create a service
-flow = InstalledAppFlow.from_client_secrets_file('client_secret.json', ['https://www.googleapis.com/auth/calendar'])
-credentials = flow.run_local_server(port=0)
+# Load environment variables
+load_dotenv()
+calendar_id = os.getenv('CALENDAR_ID')
+
+# Load credentials from file, if it exists
+credentials = None
+if os.path.exists('token.pickle'):
+    with open('token.pickle', 'rb') as token:
+        credentials = pickle.load(token)
+
+# If there are no (valid) credentials available, let the user log in.
+if not credentials or not credentials.valid:
+    if credentials and credentials.expired and credentials.refresh_token:
+        credentials.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'client_secret.json', ['https://www.googleapis.com/auth/calendar'])
+        credentials = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open('token.pickle', 'wb') as token:
+        pickle.dump(credentials, token)
+
+# Create a service
 service = build('calendar', 'v3', credentials=credentials)
 
-def get_events(service, calendar_id='primary', time_min=None, time_max=None):
-    now =datetime.now()
+def get_events(service, calendar_id=calendar_id):
+    # Get the current date and time
+    now = datetime.now(pytz.utc)
 
-    time_min = datetime(now.year, now.month, now.day) + timedelta(days=1)
+    # Set time_min as the start of tomorrow
+    time_min = datetime(now.year, now.month, now.day, tzinfo=pytz.utc) + timedelta(days=1)
+
+    # Set time_max as the end of tomorrow
     time_max = time_min + timedelta(days=1)
 
-    ## Convert datetime to RFC3339 format required by API
+    # Convert datetime objects to RFC3339 format
     time_min = time_min.isoformat()
     time_max = time_max.isoformat()
 
-    ## Call the API
+    # Call the Google Calendar API
     events_result = service.events().list(
         calendarId=calendar_id,
         timeMin=time_min,
@@ -26,15 +53,15 @@ def get_events(service, calendar_id='primary', time_min=None, time_max=None):
         singleEvents=True,
         orderBy='startTime'
     ).execute()
-    
-    ## Get all events
+
+    # Get the list of events
     events = events_result.get('items', [])
-    print(events)
 
     return events
 
 # Call the get_events function
 events = get_events(service)
+print(events)
 
 
 
