b14645a696f190c70193fce1fb5d7bf70b0a3d03
John Stoveld <js@USSRSMMPRI5C9B2.localdomain>
2023-12-27 11:20:24 -0500

    I am able to iterate through all events and mark them as calendar tasks or change the duration now.

--- app.py
+++ app.py
@@ -36,12 +36,52 @@ if overlapping_events:
 else:
     print("No overlapping events found.")
 
-# Ask the user if they want to optimize their calendar
-user_input = input("Do you want to optimize your calendar? (yes/no): ")
-if user_input.lower() == 'yes':
-    events, gap = optimizer.optimize_events(events)
-    optimizer.ensure_gaps(events, gap)
-    optimizer.convert_short_events_to_tasks(events, end_time)
-    print("Your calendar has been optimized.")
-else:
-    print("No changes have been made to your calendar.")
+try:
+    # Ask the user if they want to optimize their calendar
+    user_input = input("Do you want to optimize your calendar? (yes/no): ")
+    if user_input.lower() == 'yes':
+        tasks_to_convert = []
+
+        # Iterate through the events
+        for event in events:
+            # Ask the user if the event should be converted to a task
+            user_input = input(f"Do you want to convert the event '{event['summary']}' to a task? (yes/no): ")
+            if user_input.lower() == 'yes':
+                # Store the event for later conversion
+                tasks_to_convert.append(event)
+            else:
+                # Ask the user if the event is longer than it should be
+                user_input = input(f"Is the event '{event['summary']}' longer than it should be? (yes/no): ")
+                if user_input.lower() == 'yes':
+                    # Ask the user for the desired duration of the event
+                    while True:
+                        try:
+                            duration = int(input("Please enter the desired duration of the event in minutes (30 or 60): "))
+                            break
+                        except ValueError:
+                            print("Invalid input. Please enter a number.")
+                    # Adjust the duration of the event
+                    optimizer.adjust_event_duration(event, duration)
+
+        # Convert the selected events to tasks
+        events_copy = events.copy()
+        for task in tasks_to_convert:
+            optimizer.convert_event_to_task(task, events_copy)
+        events = events_copy
+
+        # Check for overlapping events
+        overlapping_events, gap = optimizer.check_overlaps(events)
+
+        # If there are overlapping events
+        if overlapping_events:
+            # Ask the user if they want to mark them for requiring attention
+            user_input = input("There are overlapping events. Do you want to mark them for requiring attention? (yes/no): ")
+            if user_input.lower() == 'yes':
+                # Mark the overlapping events for requiring attention
+                optimizer.mark_events_for_attention(overlapping_events)
+
+        print("Your calendar has been optimized.")
+    else:
+        print("No changes have been made to your calendar.")
+except Exception as e:
+    print(f"An error occurred: {e}")
--- optimizer.py
+++ optimizer.py
@@ -7,6 +7,35 @@ class Optimizer:
     def __init__(self, calendar):
         self.calendar = calendar
 
+    def convert_event_to_task(self, event, events):
+        # Convert the event to a task
+        task = {
+            'summary': event['summary'],
+            'start': event['start'],
+            'end': event['end'],
+            'description': event.get('description', ''),
+            'location': event.get('location', ''),
+            'status': 'task'
+        }
+        # Remove the event from the list of events
+        events.remove(event)
+        return task
+
+    def optimize_events(self, events):
+        # Implement your event optimization logic here
+        # For example, you might want to sort the events by start time:
+        events.sort(key=lambda x: x['start'].get('dateTime', x['start'].get('date')))
+
+        # Calculate the gap between events
+        gap = None
+        for i in range(1, len(events)):
+            start_i = parse(events[i]['start'].get('dateTime', events[i]['start'].get('date'))).astimezone(tzutc())
+            end_i_1 = parse(events[i-1]['end'].get('dateTime', events[i-1]['end'].get('date'))).astimezone(tzutc())
+            gap = (start_i - end_i_1).total_seconds() / 60  # Calculate gap in minutes
+
+        # Return the optimized events and the gap
+        return events, gap
+
     def check_overlaps(self, events):
         # Sort events by start time
         events.sort(key=lambda x: x['start'].get('dateTime', x['start'].get('date')))
@@ -19,31 +48,63 @@ class Optimizer:
             end_i_1 = parse(events[i-1]['end'].get('dateTime', events[i-1]['end'].get('date'))).astimezone(tzutc())
             if start_i < end_i_1:
                 overlapping_events.append([events[i-1], events[i]])
-            else:
-                gap = (start_i - end_i_1).total_seconds() / 60  # Calculate gap in minutes
+            gap = (start_i - end_i_1).total_seconds() / 60  # Calculate gap in minutes
 
         # Return the overlapping events and the gap
         return overlapping_events, gap
+
     def ensure_gaps(self, events, gap):
-        # Check if gap is less than 5 minutes
-        if gap < 5:
-            print(f"Insufficient gap of {gap} minutes")
+        # Adjust events to ensure a gap of at least 5 minutes
+        for i in range(1, len(events)):
+            start_i = parse(events[i]['start'].get('dateTime', events[i]['start'].get('date'))).astimezone(tzutc())
+            end_i_1 = parse(events[i-1]['end'].get('dateTime', events[i-1]['end'].get('date'))).astimezone(tzutc())
+            if (start_i - end_i_1).total_seconds() / 60 < gap:
+                # Adjust the start time of the current event
+                events[i]['start']['dateTime'] = (end_i_1 + timedelta(minutes=gap)).isoformat()
 
     def convert_short_events_to_tasks(self, events, end_time):
+        tasks = []
         for event in events:
             start_time = parse(event['start'].get('dateTime', event['start'].get('date'))).astimezone(tzutc())
-            end_event_time = parse(event['end'].get('dateTime', event['end'].get('date'))).astimezone(tzutc())
-            duration = (end_event_time - start_time).total_seconds() / 60  # Calculate duration in minutes
-            if duration < 30:  # If the event is shorter than 30 minutes
-                self.calendar.delete_event(event['id'])
-                self.calendar.create_task(event['summary'], event['description'])
+            end_time = parse(event['end'].get('dateTime', event['end'].get('date'))).astimezone(tzutc())
+            duration = (end_time - start_time).total_seconds() / 60  # Calculate duration in minutes
+            if duration <= 30:
+                # Convert the event to a task
+                tasks.append(event)
+                events.remove(event)
+        return tasks, events
 
     def calculate_travel_time(self, start_time, end_time):
-        # Get all events between start_time and end_time
-        events = self.calendar.get_events(start_time, end_time)
+        # Get all events between start_time and end_time and sort them by start time
+        events = sorted(self.calendar.get_events(start_time, end_time), key=lambda x: x['start'].get('dateTime', x['start'].get('date')))
 
         # Calculate travel time between events
         for i in range(1, len(events)):
-            travel_time = events[i]['start']['dateTime'] - events[i-1]['end']['dateTime']
+            travel_time = parse(events[i]['start'].get('dateTime', events[i]['start'].get('date'))).astimezone(tzutc()) - parse(events[i-1]['end'].get('dateTime', events[i-1]['end'].get('date'))).astimezone(tzutc())
             if travel_time < timedelta(minutes=15):  # less than 15 minutes
                 print(f"Not enough travel time between {events[i-1]['summary']} and {events[i]['summary']}")
+    
+    def adjust_event_duration(self, event, duration):
+        # Parse the start time of the event
+        start_time = parse(event['start'].get('dateTime', event['start'].get('date'))).astimezone(tzutc())
+        # Calculate the new end time based on the desired duration
+        end_time = start_time + timedelta(minutes=duration)
+        # Update the end time of the event
+        if 'dateTime' in event['end']:
+            event['end']['dateTime'] = end_time.isoformat()
+        else:
+            event['end']['date'] = end_time.date().isoformat()
+
+
+    def identify_short_events(self, events):
+        short_events = []
+        for event in events:
+            start_time = parse(event['start'].get('dateTime', event['start'].get('date'))).astimezone(tzutc())
+            end_time = parse(event['end'].get('dateTime', event['end'].get('date'))).astimezone(tzutc())
+            duration = (end_time - start_time).total_seconds() / 60  # Calculate duration in minutes
+            if duration <= 30:
+                # Add the event to the list of short events
+                short_events.append(event)
+        return short_events
+    
+    
